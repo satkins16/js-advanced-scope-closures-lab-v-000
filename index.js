40eca560f8261eb9b759b80f9ea2c12e2d1e3255
Scott Atkins <satkins16@gmail.com>
2017-11-14 23:12:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ function produceDrivingRange(blockRange) {
     let start = parseInt(startBlock)
     let end = parseInt(endBlock)
     let distance = Math.abs(end - start)
+    let difference = blockRange - distance
 
     if (blockRange < distance) {
       return true
     } else {
-      return false
+      return `${}`
     }
   }
 }
