566657f6c40e4e314a699f12296f2e04507eedd3
Scott Atkins <satkins16@gmail.com>
2017-11-14 23:11:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
-    let start = startBlock.parseInt(startBlock)
-    let end = endBlock.parseInt(endBlock)
+    let start = parseInt(startBlock)
+    let end = parseInt(endBlock)
     let distance = Math.abs(end - start)
 
     if (blockRange < distance) {
