b7e5f860bbcde0fcfe2b59057d2d1d75d3b32958
Scott Atkins <satkins16@gmail.com>
2017-11-14 23:45:46 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function produceDrivingRange(blockRange) {
+  return function(startBlock, endBlock) {
+    let start = parseInt(startBlock)
+    let end = parseInt(endBlock)
+    let distance = Math.abs(end - start)
+    let difference = blockRange - distance
+
+    if (difference > 0) {
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
+  }
+}
+
+function produceTipCalculator(tip) {
+  return function(fare) {
+    return fare * tip
+  }
+}
+
+function createDriver(){
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.id = ++driverId
+      this.name = name
+    }
+  }
+}
