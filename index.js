c0c711080c5323c2530f503a46234cb0d2c3bcef
Scott Atkins <satkins16@gmail.com>
2017-11-14 23:40:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,13 @@ function produceTipCalculator(tip) {
     return fare * tip
   }
 }
+
+function createDriver(){
+  let driverId = 0
+  return class {
+    constructor(name){
+      this.id = ++driverId
+      this.name = name
+    }
+  }
+}
